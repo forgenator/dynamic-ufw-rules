e4a8e620243063a40d1e8523e5f165c682bd04c6
forgenator <forgenator@users.noreply.github.com>
2024-05-03 16:49:32 +0300

    Small script changes. Add timestamp and log file output.

--- dynamicdnsupdater.sh
+++ dynamicdnsupdater.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Configuration
-HOSTNAME=hostname.duckdns.org
+HOSTNAME=host.domainname.tld
 SSH_PORT=22
+TIMESTAMP=`date "+%Y-%m-%dT%H:%M:%S"`
 
 # Cronjob
 #*/15 * * * * root bash /path/to/dynamicdnsupdater.sh
@@ -13,16 +14,17 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Variables
-new_ip=$(host $HOSTNAME | head -n1 | cut -f4 -d ' ')
+new_ip=$(host $HOSTNAME | sed -n 2p | cut -f4 -d ' ')
 old_ip=$(/usr/sbin/ufw status | grep $HOSTNAME | head -n1 | tr -s ' ' | cut -f3 -d ' ')
 
 # Logic
 if [ "$new_ip" = "$old_ip" ] ; then
-    echo IP address has not changed
+    echo "$TIMESTAMP IP address has not changed - $old_ip" >> /var/log/dynamicdnsupdater.log
 else
     if [ -n "$old_ip" ] ; then
         /usr/sbin/ufw delete allow from $old_ip to any port $SSH_PORT
     fi
     /usr/sbin/ufw allow from $new_ip to any port $SSH_PORT comment $HOSTNAME
-    echo iptables have been updated
+    echo "$TIMESTAMP IP address has changed, UFW set from $old_ip to $new_ip" >> /var/log/dynamicdnsupdater.log
+
 fi
