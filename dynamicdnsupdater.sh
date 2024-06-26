65aa94195bd273576d653500af6a59ca8b5646d7
forgenator <forgenator@users.noreply.github.com>
2024-06-26 12:43:43 +0300

    Update dynamicdnsupdater.sh
    Add check to make sure our ip address is valid, mainly to use if host command returns invalid data for timeout or similar.

--- dynamicdnsupdater.sh
+++ dynamicdnsupdater.sh
@@ -5,7 +5,7 @@ HOSTNAME=host.domainname.tld
 SSH_PORT=22
 TIMESTAMP=`date "+%Y-%m-%dT%H:%M:%S"`
 
-# Cronjob
+# Cronjob for every 15min
 #*/15 * * * * root bash /path/to/dynamicdnsupdater.sh
 
 if [[ $EUID -ne 0 ]]; then
@@ -17,14 +17,18 @@ fi
 new_ip=$(host $HOSTNAME | sed -n 2p | cut -f4 -d ' ')
 old_ip=$(/usr/sbin/ufw status | grep $HOSTNAME | head -n1 | tr -s ' ' | cut -f3 -d ' ')
 
-# Logic
-if [ "$new_ip" = "$old_ip" ] ; then
+# Check if new_ip is valid ip address
+if [[ "$new_ip" =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$ ]]; then
+  # Logic for updating ufw rules or not
+  if [ "$new_ip" = "$old_ip" ] ; then
     echo "$TIMESTAMP IP address has not changed - $old_ip" >> /var/log/dynamicdnsupdater.log
-else
+  else
     if [ -n "$old_ip" ] ; then
         /usr/sbin/ufw delete allow from $old_ip to any port $SSH_PORT
     fi
     /usr/sbin/ufw allow from $new_ip to any port $SSH_PORT comment $HOSTNAME
-    echo "$TIMESTAMP IP address has changed, UFW set from $old_ip to $new_ip" >> /var/log/dynamicdnsupdater.log
-
+    echo "$TIMESTAMP IP has changed, UFW set from $old_ip to $new_ip" >> /var/log/dynamicdnsupdater.log
+  fi
+else
+  echo "Not valid IP - $new_ip" >> /var/log/dynamicdnsupdater.log
 fi
